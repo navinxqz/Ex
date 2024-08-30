11073027fb9bc12ff982409eb8ec9e8407cdec18
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-31 01:18:05 +0600

    RoleCls

---
+++ Classes/RoleCls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdminSystem.Classes
+{
+    public class RoleCls
+    {
+        public int Id { get; set; }
+        public int EnrollmentNo { get; set; }
+        public int MaxEnrollment { get; set; }
+        public int Pricing { get; set; }
+        public string Name { get; set; }
+        public string SessionDay1 { get; set; }
+        public string SessionDay2 { get; set; }
+        public bool Status { get; set; }
+        //public TrainerModel? TrainerModel { get; set; }
+
+        //private 
+        public RoleCls(int id=0, int enrollmentNo=0,int maxEnrollment=0,int pricing=0,
+           string name=null, string sessionDay1=null, string sessionDay2 = null, bool status = false)
+        {
+            Id = id;
+            EnrollmentNo = enrollmentNo;
+            MaxEnrollment = maxEnrollment;
+            Pricing = pricing;
+            Name = name;
+            SessionDay1 = sessionDay1;
+            SessionDay2 = sessionDay2;
+            Status = status;
+            //trainers in future...
+        }
+
+
+    }
+}
---
+++ Classes/Trainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdminSystem.Classes
+{
+    public class Trainer : Person
+    {
+        public string Specialized {  get; set; }
+        public int LessonPrice {  get; set; }
+        public bool Status {  get; set; }
+
+
+    }
+}
