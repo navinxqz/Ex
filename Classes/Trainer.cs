7084bbf52d468f77cd44e3c52dcf41780946d7a6
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-31 01:30:09 +0600

    Generate Id for trainers

--- Classes/Trainer.cs
+++ Classes/Trainer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ComponentFactory.Krypton.Toolkit;
+using System;
+using System.Drawing;
 
 namespace AdminSystem.Classes
 {
@@ -8,6 +10,32 @@ namespace AdminSystem.Classes
         public int LessonPrice {  get; set; }
         public bool Status {  get; set; }
 
+        public Trainer(int id =0, string firstname=null, string lastname = null, string gender = null, string imgbase = null, string email = null, string phone = null, string password = null, DateTime birthday=default, Image pic =null, string specialized = null, int lessonPrice=0, bool status=false) 
+            : base(id, firstname, lastname, gender, imgbase, email, phone, password, birthday, pic)
+        { 
+            Specialized = specialized;
+            LessonPrice = lessonPrice;
+            Status = status;
+        }
 
+        public override int GenerateID()
+        {
+            int year = (DateTime.Now.Year % 100) + 3;
+            int baseId = year * 1000000; // Create a base ID with the prefix.
+            int newId;
+
+            /*int lastId = StaticClass.trainerService.getLastId();
+            if (lastId == -1)
+            {
+                Console.WriteLine("Error: Unable to get last ID.");
+                return -1;
+            }
+
+            // Determine new ID based on last ID
+            int newId = (lastId == 0 || (lastId / 1000000) != (baseId / 1000000)) ? baseId + 1 : lastId + 1;    */
+
+            this.id = newId;
+            return newId;
+        }
     }
 }
