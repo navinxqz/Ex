54c4dacfe6d1610c6b2328c66a4efeb76b96f4cf
navin-nawshin <navinmdnawshin@gmail.com>
2024-09-01 22:11:23 +0600

    Trainer Query added

---
+++ Query/TrainerQuery.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdminSystem.Classes;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace AdminSystem.Query
+{
+    public class TrainerQuery
+    {
+        public List<Trainer> Search(string search, bool incPic, bool byId = false, bool byFulName = false) {
+            try
+            {
+                List<Trainer> trainers = new List<Trainer>();
+                string query = "";
+
+                if(byId && int.TryParse(search, out int id))
+                {
+                    query = $"SELECT {GetCol(incPic)} FROM ADMINSYSTEM.TRAINER WHERE ID = {id}";
+                }else if (byFulName)
+                {
+                    query = $"SELECT {GetCol(incPic)} FROM ADMINSYSTEM.TRAINER CONCAT(FIRSTNAME, ' ', LASTNAME) = '{search}'";
+                }
+
+                if (string.IsNullOrEmpty(query))
+                {
+                    Console.WriteLine("Error! Nothing to search");
+                    return null;
+                }
+
+                //using(MySqlConnection con = new MySqlConnection())
+
+                MySqlDataReader read = StaticClass.sql.MySqlSelect(query);
+
+                if (read.HasRows)
+                {
+                    while (read.Read())
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        } 
+    }
+}
